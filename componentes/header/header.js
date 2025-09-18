03eafa7f1cfb5c0e43e04bc0262d896d11164652
alejoss <aggomez@scl.edu.gt>
2025-09-17 19:43:11 -0600

    Add files via upload

--- componentes/header/header.js
+++ componentes/header/header.js
@@ -1,7 +1,24 @@
+<<<<<<< HEAD
+export function obtenerHeader() {
+    let header = document.createElement("header");
+    header.className = "header";
+
+    let titulo = document.createElement("h1");
+    titulo.textContent = "Lista de Compras";
+
+    let divDownload = document.createElement("div");
+    divDownload.id = "download";
+
+    header.appendChild(titulo);
+    header.appendChild(divDownload);
+
+    return header;
+=======
 // componentes/header/header.js
 export function obtenerHeader() {
     let header = document.createElement("header");
     header.className = "header";
     header.textContent = "Lista de Compras";
    return header;
+>>>>>>> 12079b65073084366c0de7ef09a45cb8a0db12b4
 }
--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import { obtenerHeader } from "./componentes/header/header.js";
 import { obtenerSection } from "./componentes/section/section.js";
+<<<<<<< HEAD
+import { loadHtmlSectionDownload } from "./control/descarga/seccionDescarga.js";
+=======
+>>>>>>> 12079b65073084366c0de7ef09a45cb8a0db12b4
 
 function lista() {
     let paginaLista = document.createElement("div");
     paginaLista.className = "pagina-lista";
+<<<<<<< HEAD
+    paginaLista.id = "root";
+=======
+>>>>>>> 12079b65073084366c0de7ef09a45cb8a0db12b4
 
     paginaLista.appendChild(obtenerHeader());
     paginaLista.appendChild(obtenerSection());
@@ -12,3 +20,7 @@ function lista() {
 }
 
 document.body.appendChild(lista());
+<<<<<<< HEAD
+loadHtmlSectionDownload();
+=======
+>>>>>>> 12079b65073084366c0de7ef09a45cb8a0db12b4
